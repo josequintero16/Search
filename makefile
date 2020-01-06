d2fe29e465dfb590d30bbdf74c8b8afba61a839d
joseQ <joseqfyk@gmail.com>
2020-01-06 00:21:15 -0600

    Fixed algorithm

---
+++ makefile
@@ -0,0 +1,5 @@
+search: search.c
+	gcc -o search search.c
+
+clean:
+	rm search 
--- search.c
+++ search.c
@@ -1,29 +1,39 @@
+//
+//includes
+//
+
 #include <stdio.h>
 
 //
 //functions
 //
 
-int binSrcI(int Arr[], int x); // iterative binary search
+int binSrcI(int Arr[],int size, int x); // iterative binary search
+
+//
+//main
+//
 
 int main()
 {
 	int A[] = {0,2,4,6,8,10};
-	int src = 6;
-	int index = binSrcI(A,src);
-	printf("The index of the number searched for is %6d\n", index);
+	int src;
+	printf("Which number would you like to search?\n");
+	scanf("%d",&src);
+	int index = binSrcI(A,sizeof(A)/sizeof(A[0]),src);
+	(index == -1) ? printf("Number was not found in the array.\n")
+	              : printf("The index of the number searched for is %d.\n", index);
 	return 0;
 }
 
-int binSrcI(int Arr[], int x)
+int binSrcI(int Arr[],int size, int x)
 {
-	int n = 6; 
 	int low = 0;
-	int hi = n - 1;
+	int hi = size - 1;
 	int m;
 	while(low <= hi)
 	{
-		m = low + (hi + 1) / 2;
+		m = low + (hi - low) / 2;
 
 		if(x == Arr[m]) return m;
 
